4c96e3c751e7f6cb4ed62b9e7b0ddb1deb7835c5
RISING-CODER777 <85391175+RISING-CODER777@users.noreply.github.com>
2023-11-05 19:41:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <title>Forum Driver efficiency</title>
+      </head>
+      <iframe
+        title="Forum Driver efficiency"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=4a6458e3-e0e3-427b-9e91-ad5957cf34ab&autoAuth=true&ctid=3e19468b-e065-44a1-9cde-749457855c06"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
